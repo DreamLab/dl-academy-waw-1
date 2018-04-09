e3ac0a47b88296c71d536cc7b2ad04b754ace601
Swacha Rafał <Rafal.Swacha@dreamlab.pl>
2018-04-09 14:34:56 +0200

    case fix

--- _additional/fakeClient/fakeClient.js
+++ _additional/fakeClient/fakeClient.js
@@ -1,12 +1,12 @@
 const _ = require('lodash');
 
 const ClientHandler = require('./lib/ClientHandler');
-const fileReader = require('./lib/fileReader');
+const FileReader = require('./lib/FileReader');
 
 const CLIENTS_PER_SERVER = 3;
 const clients = [];
 
-_.forEach(fileReader.read('clients.txt'), clientAddress => {
+_.forEach(FileReader.read('clients.txt'), clientAddress => {
 	for (let i = 0; i < CLIENTS_PER_SERVER; i++) {
 		clients.push(new ClientHandler(clientAddress));
 	}
