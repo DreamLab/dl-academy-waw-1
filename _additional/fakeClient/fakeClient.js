31912e2ce1bbac0908791e8acae9390a9db114b1
Swacha Rafał <Rafal.Swacha@dreamlab.pl>
2018-04-09 14:53:02 +0200

    small name fixes

--- _additional/fakeClient/fakeClient.js
+++ _additional/fakeClient/fakeClient.js
@@ -6,12 +6,14 @@ const FileReader = require('./lib/FileReader');
 const CLIENTS_PER_SERVER = 3;
 const clients = [];
 
-_.forEach(FileReader.read('clients.txt'), clientAddress => {
+// Read clients.txt file, create new client handler and push it to the array.
+_.forEach(FileReader.read('servers.txt'), serverAddress => {
 	for (let i = 0; i < CLIENTS_PER_SERVER; i++) {
-		clients.push(new ClientHandler(clientAddress));
+		clients.push(new ClientHandler(serverAddress));
 	}
 });
 
+// Run all clients.
 _.forEach(clients, client => client.run());
 
 
