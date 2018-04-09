e8908e7d37e2bafed6fbafdbefc588c7776a3437
Iwo Czerniawski <iwo.czerniawski@dreamlab.pl>
2018-04-09 12:54:30 +0200

    Spaces

--- step3/app.js
+++ step3/app.js
@@ -6,9 +6,9 @@ const router = require('./router');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', router);
-app.use((req, res) => res.status(404).send({ error: 'Not found'}));
+app.use((req, res) => res.status(404).send({error: 'Not found'}));
 
 app.listen(3000, () => console.log('App listening on port 3000!'));
 
