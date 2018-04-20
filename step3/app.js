c7f8c71a0272acdde8fe73570131168bfe5dc249
Spaceinvaderr <m.chirillova@gmail.com>
2018-04-20 15:12:08 +0200

    updates

--- step3/app.js
+++ step3/app.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/api/', router);
-app.use(express.static('../_additional/frontend'));
+app.use('/', router);
+app.use(express.static('../_additional/frontend/step3'));
 
 app.use((req, res) => res.status(404).send({error: 'Not found'}));
 
